d30ccff57ff6fb22db5e7da91d8dcc0b2fef2c34
Nelson Alexandrino <nasson.alexander@gmail.com>
2019-08-25 12:44:56 +0200

    dklkskslf

---
+++ .history/Histograms_20190825124451.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as pyplot
+
+with open('Goals.txt', 'r') as file:
+    HomeTeamGoals = [int(x) for x in file.readline().strip(
+        '\n').strip(' ').split(' ')]
+    AwayTeamGoals = [int(x) for x in file.readline().strip(
+        '\n').strip(' ').split(' ')]
